50297af8b38e369e6cd57bae362b79b4321e89d2
Radu Cazacu <cazacuraduic@gmail.com>
2021-04-08 13:40:13 +0300

    Added redux

--- front/src/actions/productActions.js
+++ front/src/actions/productActions.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
 import axios from "axios";
@@ -16,6 +19,24 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.message && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listProductDetails = (ID) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST,
+    });
+    const { data } = await axios.get(`/api/products/${ID}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.message && error.response.data.message
           ? error.response.data.message
